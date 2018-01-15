1dd18948b92c4e85207609a85f1da15a9d4e16b9
ethan ozelius <e.ozelius@gmail.com>
2018-01-15 13:58:01 -0500

    implement jumpTo(step)

--- src/index.js
+++ src/index.js
@@ -49,14 +49,15 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext: true,
     };
   }
 
   handleClick(i) {
-    const history = this.state.history
+    const history = this.state.history.slice(0, this.state.stepNumber + 1)
     const current = history[history.length - 1]
-    const squares = current.squares.slice()    
+    const squares = current.squares.slice()
     if(calculateWinner(squares) || squares[i]){
       return;
     }
@@ -65,15 +66,37 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
+    })
+  }
+
+  jumpTo(step){
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
     })
   }
 
   render() {
     const history = this.state.history
-    const current = history[history.length - 1]
+    const current = history[this.state.stepNumber]
     const winner  = calculateWinner(current.squares)
     let status;
+
+    const moves = history.map((step, move) => {
+      let desc
+      if(move){
+        desc = `Go to move # ${move}`
+      } else {
+        desc = `Go to Start`
+      }
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      )
+    })
 
     if(winner) {
       status = `Winner: ${winner}`
@@ -90,7 +113,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ul>{moves}</ul>
         </div>
       </div>
     );
